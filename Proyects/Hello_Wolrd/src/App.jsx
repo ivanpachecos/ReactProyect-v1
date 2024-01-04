b3e9d1c3afd7445d6cc6f94ac2c0df16c0a9371c
Ivan Pacheco <ivanpachecos@yahoo.com>
2024-01-04 18:33:37 -0500

    Hello app Twitter Component

--- Proyects/Hello_Wolrd/src/App.jsx
+++ Proyects/Hello_Wolrd/src/App.jsx
@@ -1,11 +1,52 @@
 import TwiterFollowCard from "./components/TwiterFollowCard"
 import './App.css'
+
 const App = () => {
+
+  const users = [
+    {
+      name: 'Ivan Pacheco',
+      userName: 'leo',
+      email: 'ivanleonardo@gmail.com',
+      isFollowing: true,
+    },
+
+    {
+      name: 'Alex Francisco',
+      userName: 'alex',
+      email: 'alexZfrancisco@gmail.com',
+      isFollowing: false,
+    },
+
+    {
+      name: 'Steven Zambrano',
+      userName: 'messi',
+      email: 'stevenZambrano.com',
+      isFollowing: true,
+    },
+
+    {
+      name: 'Cristiano Palacios',
+      userName: 'cristiano',
+      email: 'cristiamp2322@gmail.com',
+      isFollowing: false,
+    },
+
+  ]
+
   return (
-    <>
-      <TwiterFollowCard name={'Miguel Angel'} userName={'miguel'} />
-      <TwiterFollowCard name={'Alex Rosario'} userName={'alex'} />
-    </>
+    <section className="App">
+      {
+        users.map(({ name, userName, isFollowing }) => (
+          <TwiterFollowCard 
+          key={userName} 
+          userName={userName} 
+          isFollowing={isFollowing}>
+            {name}
+          </TwiterFollowCard>
+        ))
+      }
+    </section>
   )
 }
 
--- Proyects/Hello_Wolrd/src/components/TwiterFollowCard.jsx
+++ Proyects/Hello_Wolrd/src/components/TwiterFollowCard.jsx
@@ -1,22 +1,38 @@
+import { useState } from "react"
 
-const TwiterFollowCard = ({ name, userName, isFollow }) => {
+
+
+const TwiterFollowCard = ({ children, userName, initialIsFollowing }) => {
+
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+  const text = isFollowing ? 'siguiendo' : 'seguir'
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing)
+  }
+
+  const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
-        <img 
-        className="tw-followCard-avatar" 
-        src={`https://unavatar.io/${userName}`} 
-        alt="" />
+        <img
+          className="tw-followCard-avatar"
+          src={`https://unavatar.io/${userName}`}
+          alt="" />
         <div className="tw-followCard-info ">
-          <strong>{name}</strong>
-          <span className="tw-followCard-infoUserName">{userName}</span>
+          <strong>{children}</strong>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
         </div>
-        <aside>
-          <button className="tw-followCard-button">
-            Me gusta
-          </button>
-        </aside>
       </header>
+      <aside>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className='tw-followCard-text'>{text}</span>
+          <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+        </button>
+      </aside>
     </article>
   )
 }
